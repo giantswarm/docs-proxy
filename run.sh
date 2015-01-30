7d972014825c3331db2d0f1520a24d4b9bc5ac2a
marian <marian@giantswarm.io>
2015-01-30 13:38:39 +0100

    Renamed env vars

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 echo "Environment variables for nginx:"
-echo "MKDOCSMASTER_PORT_8000_TCP_ADDR: ${MKDOCSMASTER_PORT_8000_TCP_ADDR}"
-echo "MKDOCSMASTER_PORT_8000_TCP_PORT: ${MKDOCSMASTER_PORT_8000_TCP_PORT}"
-echo "MKDOCSSLAVE_PORT_8000_TCP_ADDR: ${MKDOCSSLAVE_PORT_8000_TCP_ADDR}"
-echo "MKDOCSSLAVE_PORT_8000_TCP_PORT: ${MKDOCSSLAVE_PORT_8000_TCP_PORT}"
+echo "DOCSMASTER_PORT_8000_TCP_ADDR: ${DOCSMASTER_PORT_8000_TCP_ADDR}"
+echo "DOCSMASTER_PORT_8000_TCP_PORT: ${DOCSMASTER_PORT_8000_TCP_PORT}"
+echo "DOCSSLAVE_PORT_8000_TCP_ADDR: ${DOCSSLAVE_PORT_8000_TCP_ADDR}"
+echo "DOCSSLAVE_PORT_8000_TCP_PORT: ${DOCSSLAVE_PORT_8000_TCP_PORT}"
 echo "SITESEARCH_PORT_9200_TCP_ADDR: ${SITESEARCH_PORT_9200_TCP_ADDR}"
 echo "SITESEARCH_PORT_9200_TCP_PORT: ${SITESEARCH_PORT_9200_TCP_PORT}"
 
-# set MKDOCSMASTER ip/port in nginx conf
-sed -i "s/\%\%MKDOCSMASTER_IP\%\%/${MKDOCSMASTER_PORT_8000_TCP_ADDR}/" /etc/nginx/nginx.conf
-sed -i "s/\%\%MKDOCSMASTER_PORT\%\%/${MKDOCSMASTER_PORT_8000_TCP_PORT}/" /etc/nginx/nginx.conf
+# set DOCS_MASTER ip/port in nginx conf
+sed -i "s/\%\%DOCS_MASTER_IP\%\%/${DOCSMASTER_PORT_8000_TCP_ADDR}/" /etc/nginx/nginx.conf
+sed -i "s/\%\%DOCS_MASTER_PORT\%\%/${DOCSMASTER_PORT_8000_TCP_PORT}/" /etc/nginx/nginx.conf
 
-# set MKDOCSSLAVE ip/port in nginx conf
-sed -i "s/\%\%MKDOCSSLAVE_IP\%\%/${MKDOCSSLAVE_PORT_8000_TCP_ADDR}/" /etc/nginx/nginx.conf
-sed -i "s/\%\%MKDOCSSLAVE_PORT\%\%/${MKDOCSSLAVE_PORT_8000_TCP_PORT}/" /etc/nginx/nginx.conf
+# set DOCS_SLAVE ip/port in nginx conf
+sed -i "s/\%\%DOCS_SLAVE_IP\%\%/${DOCSSLAVE_PORT_8000_TCP_ADDR}/" /etc/nginx/nginx.conf
+sed -i "s/\%\%DOCS_SLAVE_PORT\%\%/${DOCSSLAVE_PORT_8000_TCP_PORT}/" /etc/nginx/nginx.conf
 
 # set SITESEARCH ip/port in nginx conf
 sed -i "s/\%\%SITESEARCH_IP\%\%/${SITESEARCH_PORT_9200_TCP_ADDR}/" /etc/nginx/nginx.conf
